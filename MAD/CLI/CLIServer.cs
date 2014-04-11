622dca278e729952afc9b12efe55a38402c346df
onixion <alin.porcic@gmail.com>
2014-04-12 00:58:31 +0200

    Change dir structure.

---
+++ MAD/CLI/CLIServer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace MAD
+{
+    public abstract class CLIServer()
+    {
+        public string version = "0.0.0.1";
+
+        private TcpListener tcpListener;
+        private TcpClient tcpClient;
+        private Thread listenerThread;
+
+        public void Init()
+        {
+            listenerThread = new Thread(WaitForClients);
+        }
+
+        public void Start()
+        {
+            if (listenerThread.ThreadState == ThreadState.Unstarted || listenerThread.ThreadState == ThreadState.Stopped)
+            {
+                listenerThread.Start();
+            }
+        }
+
+        public void Stop()
+        {
+            if (listenerThread.ThreadState == ThreadState.Running)
+            {
+                listenerThread.Abort();
+            }
+        }
+
+        private void WaitForClients()
+        {
+            while (true)
+            {
+                tcpClient = tcpListener.AcceptTcpClient();
+            }
+        }
+    }
+}
