df9e4413fe2c9db854d3cc160cf23b1b57a2b3e0
onixion <alin.porcic@gmail.com>
2014-06-23 11:42:04 +0200

    v0.0.2.8 RC2

--- MAD/Program.cs
+++ MAD/Program.cs
@@ -8,7 +8,7 @@ namespace MAD
 {
     class Program
     {
-        /* MAD - Network Monitoring v0.0.2.9 RC1 */
+        /* MAD - Network Monitoring v0.0.2.9 RC2 */
 
         private const string dataPath = "data";
 
