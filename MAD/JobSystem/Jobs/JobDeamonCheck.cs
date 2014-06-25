91a297b1f1ec78a2b065264a7163aacebffffae3
Tarkes <dani-san@chello.at>
2014-06-25 23:04:47 +0200

    initial commit for new Job

---
+++ MAD/JobSystem/Jobs/JobDeamonCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MAD.JobSystem.Jobs
+{
+    class JobDeamonCheck
+    {
+        #region members
+
+        #endregion
+
+        #region constructors
+
+        #endregion
+
+        #region methods
+
+        #endregion
+    }
+}
