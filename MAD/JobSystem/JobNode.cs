65b20f2ea534b552f51faa8c1356d1ade298faec
onixion <alin.porcic@gmail.com>
2014-07-04 15:43:31 +0200

    Added JobNode

---
+++ MAD/JobSystem/JobNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace MAD.JobSystemCore
+{
+    public class JobNode
+    {
+        private static int _idCount = 0;
+        private int _id;
+        private object _initIDLock = new object();
+
+        private PhysicalAddress _macAddress;
+        private IPAddress _ipAddress;
+
+        public List<Job> _jobs;
+
+        private void InitID()
+        {
+            lock (_initIDLock)
+            {
+                _id = _idCount;
+                _idCount++;
+            }
+        }
+    }
+}
