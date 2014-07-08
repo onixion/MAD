e0d210ce416ebd98d0c65924e76c314acf90c71a
Tarkes <dani-san@chello.at>
2014-07-09 00:09:15 +0200

    including dhcpReader

--- MAD/DHCPReader/DHCPReader.cs
+++ MAD/DHCPReader/DHCPReader.cs
@@ -8,9 +8,34 @@ using MAD.Helper;
 
 namespace MAD.DHCPReader
 {
-    class DHCPReader
+    public class DHCPReader
     {
-        NetworkHelper helper = new NetworkHelper();
+        private static NetworkHelper helper = new NetworkHelper();
+        private IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, 67);
+        private uint threadCnt = 0;
+        private byte[] data;
+        private uint maxThreads = 100;
 
+        public void Execute()
+        {
+            
+        }
+
+        private void CatchDHCP()
+        {
+            UdpClient listener = new UdpClient(67);
+            data = listener.Receive(ref groupEP);
+            listener.Close();
+        }
+
+        private void StartThread()
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ProcessData));
+        }
+
+        private void ProcessData(Object stateInfo)
+        {
+
+        }
     }
 }
