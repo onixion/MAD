01b27a1412cbcc1b69d15ee6bb730d78ada040e1
wetfire8 <megarambo8@gmail.com>
2014-08-02 21:26:43 +0200

    new start with methodes - create the db

--- MAD/DB.cs
+++ MAD/DB.cs
@@ -1,9 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Data.SQLite; //include SQLite database library
+using System.IO;  
 
 namespace MAD
 {
-    class DB
+    public class DB
     {
+        private string _DBname = "MyDatabase.sqlite";
+        private SQLiteConnection _dbConnection;
+
+        public DB(string filename)
+        {
+            if (!File.Exists(_DBname))
+            {
+                SQLiteConnection.CreateFile(_DBname); 
+            }
+        }
+        public void Connect()
+        {
+            _dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            _dbConnection.Open();
+        }
+
+        public void CreateDB(string DBname)
+        {
+           if (_dbConnection.State == SQLiteConnection. )
+        }
     }
 }
