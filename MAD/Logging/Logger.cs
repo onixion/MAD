4129d327d39b7d44b01b8ce2269d456b32833e04
Tarkes <dani-san@chello.at>
2014-08-22 00:49:57 +0200

    start implementing logger

---
+++ MAD/Logging/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace MAD.Logging
+{
+    public static class Logger
+    {
+        public static string logFile; 
+
+        private static bool PathFileExists()
+        {
+            string _pathToPathFile = Directory.GetCurrentDirectory();
+            string _content = "lorem ipsum";
+            if (File.Exists(_pathToPathFile + @"/path.txt"))
+            {
+                using (StreamReader sr = new StreamReader(_pathToPathFile + @"/path.txt"))
+                    _content = sr.ReadToEnd();
+
+                if (!String.IsNullOrEmpty(_content))
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        public static void CreateNewPathFile()
+        {
+            string _newPathToPathFile = Path.GetFullPath("./");
+            string _pathToLogFile = Path.GetFullPath("./") + @"../../";
+            File.Create(_newPathToPathFile + @"/path.txt");
+
+            using (StreamWriter sw = new StreamWriter(_newPathToPathFile + @"/path.txt"))
+                sw.WriteLine(_pathToLogFile);
+        }
+    }
+}
