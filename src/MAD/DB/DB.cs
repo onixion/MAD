ce9804f35c8d4b5ce789bcaf2991a6c54a08b5bd
wetfire8 <megarambo8@gmail.com>
2014-12-14 18:59:33 +0100

    don t use join

--- src/MAD/DB/DB.cs
+++ src/MAD/DB/DB.cs
@@ -10,27 +10,6 @@ using System.Data;
 using MAD.JobSystemCore;
 using MAD.Logging;
 
-/*
- //add to main!!
-            DB MADstore = new DB("MAD.sqlite");
-            MADstore.ConnectDB("MAD.sqlite");
-
-            MADstore.CreateDeviceTable();
-            MADstore.CreateEventTable();
-            MADstore.CreateJobTypeTable();
-            MADstore.CreateProtocolTable();
-            MADstore.CreateStatusTable();
-            MADstore.CreateSummaryTable();
- 
-            MADstore.DisconnectDB();
- */
-//how to insert e.g. ... 
-/*
-MADstore.InsertToTable("Device_Table", new Insert("GUID", "0001"), new Insert("HOST", "YOLO"), new Insert("IP", "192.168.17.17"));
-                          Tablename                column    value ...
-*/
-//reading function will be finished soon
-
 
 namespace MAD.Database
 { 
@@ -40,9 +19,9 @@ namespace MAD.Database
 
         public DB(string DBname)
         {
-            // ob datei existiert
+            // if file exists
             SQLiteConnection.CreateFile(DBname);
-            // verbinden
+            // connect
             ConnectDB(DBname);
 
             using (SQLiteCommand _command = new SQLiteCommand(_con))
@@ -52,8 +31,27 @@ namespace MAD.Database
                 _command.ExecuteNonQuery();
 
                 // DeviceTable
-                _command.CommandText = "";
+                _command.CommandText = "create table if not exists DeviceTable (ID_NODE integer, HOSTNAME text, IP text, MAC text, ONLINE integer, MEMO1 varchar(5), MEMO2 text);";
                 _command.ExecuteNonQuery(); // TODO
+
+                _command.CommandText = "create table if not exists JobTable (ID_NODE integer, ID_JOB integer, JOBNAME integer, JOBTYPE integer, OUTSTATE integer, STARTTIME text, STOPTIME text, DELAYTIME text, OUTDESC text);";
+                _command.ExecuteNonQuery();
+
+                _command.CommandText = "create table if not exists JobNameTable (JOB_ID integer auto increment primary key, NAME text);";
+                _command.ExecuteNonQuery();
+
+                _command.CommandText = "create table if not exists JobTypeTable (JOBTYPE_ID integer auto increment primary key, JOBTYPE text)";
+                _command.ExecuteNonQuery();
+                /*
+                _command.CommandText = "create table if not exists ProtocolTable (Protocol_ID integer auto increment primary key, Protocol text)";
+                _command.ExecuteNonQuery();
+                */
+
+                _command.CommandText = "create table if not exists OutStateTable (ID INTEGER auto increment primary key, OUTSTATE TEXT);";
+                _command.ExecuteNonQuery();
+                
+                _command.CommandText = "create table if not exists SummaryTable (ID_NODE integer, STARTTIME text, STOPTIME text, SUCCESS text, AVERAGE_DURATION text, MAX_DURATION text, MIN_DURATION text);";
+                _command.ExecuteNonQuery();
             }
         }
 
@@ -68,61 +66,7 @@ namespace MAD.Database
             _con.Close();
         }
 
-        /*
-        public void CreateDeviceTable()
-        {
-            string sql = "CREATE TABLE IF NOT EXISTS Device_Table ( GUID TEXT, HOSTNAME TEXT, IP TEXT, MAC TEXT, Online INTEGER, Memo1 VARCHAR(5), Memo2 TEXT);";
 
-            SQLiteCommand command = new SQLiteCommand(sql, _con);
-            command.ExecuteNonQuery();
-            command.Dispose();
-        }
-
-        public void CreateEventTable()
-        {
-            string sql = "CREATE TABLE IF NOT EXISTS Event_Table ( GUID TEXT, JOBNAME INTEGER, JOBTYPE INTEGER, PROTOCOL INTEGER, SUCCESS INTEGER, StartTime TEXT, StopTime TEXT, DelayTime TEXT, Custom1 TEXT, Custom2 INTEGER);";
-            
-            SQLiteCommand command = new SQLiteCommand(sql, _dbConnection);
-            command.ExecuteNonQuery();
-            command.Dispose();
-        }
-
-        public void CreateJobTypeTable()
-        {
-            string sql = "CREATE TABLE IF NOT EXISTS Job_Type_Table ( ID INTEGER PRIMARY KEY AUTOINCREMENT, JobType TEXT);";
-
-            SQLiteCommand command = new SQLiteCommand(sql, _dbConnection);
-            command.ExecuteNonQuery();
-            command.Dispose();
-        }
-
-        public void CreateJobNameTable()
-        {
-            string sql = "CREATE TABLE IF NOT EXISTS Job_Name_Table ( ID INTEGER PRIMARY KEY AUTOINCREMENT, JobNames TEXT);";
-
-            SQLiteCommand command = new SQLiteCommand(sql, _dbConnection);
-            command.ExecuteNonQuery();
-            command.Dispose();
-        }
-
-        public void CreateProtocolTable()
-        {
-            string sql = "CREATE TABLE IF NOT EXISTS Protocol_Table ( ID INTEGER PRIMARY KEY AUTOINCREMENT, Protocol TEXT);";
-
-            SQLiteCommand command = new SQLiteCommand(sql, _dbConnection);
-            command.ExecuteNonQuery();
-            command.Dispose();
-        }
-
-        public void CreateSummaryTable()
-        {
-            string sql = "CREATE TABLE IF NOT EXISTS Summary_Table ( GUID TEXT, DATE TEXT, JOBTYPE INTEGER, PROTOCOL INTEGER, OutState INTEGER);";
-            
-            SQLiteCommand command = new SQLiteCommand(sql, _dbConnection);
-            command.ExecuteNonQuery();
-            command.Dispose();
-        }
-        */
         //insert data
         public void InsertToTable(string TableName, params Insert[] insdata)
         {
@@ -144,7 +88,7 @@ namespace MAD.Database
             }
             sql += "')";
 
-            SQLiteCommand command = new SQLiteCommand(sql, _dbConnection);
+            SQLiteCommand command = new SQLiteCommand(sql, _con);
             command.ExecuteNonQuery();
             command.Dispose();
         }
@@ -153,7 +97,7 @@ namespace MAD.Database
         public DataTable ReadTable(string TableName)
         {
             string sql = "SELECT * FROM " + TableName + "";
-            SQLiteCommand command = new SQLiteCommand(sql, _dbConnection);
+            SQLiteCommand command = new SQLiteCommand(sql, _con);
             SQLiteDataReader reader = command.ExecuteReader();
             DataTable TempResult = new DataTable();
             command.Dispose();
@@ -165,7 +109,7 @@ namespace MAD.Database
         public DataTable ReadResult(string TableName, int TableID)
         {
             string sql = "SELECT * FROM " + TableName + " WHERE ID='" + TableID + "'";
-            SQLiteCommand command = new SQLiteCommand(sql, _dbConnection);
+            SQLiteCommand command = new SQLiteCommand(sql, _con);
             SQLiteDataReader reader = command.ExecuteReader();
             DataTable TempResult = new DataTable();
             TempResult.Load(reader);
@@ -177,7 +121,7 @@ namespace MAD.Database
             //string sql = "SELECT GUID, JobNames FROM " + TableName + " INNER JOIN Job_Name_Table ON Event_Table.JOBNAME = Job_Name_Table.JobNames WHERE GUID='" + TableID + "'";
             //string sql = "SELECT * FROM Job_Name_Table, Job_Type_Table where Job_Name_Table.ID = Job_Type_Table.ID";
             string sql = "SELECT * FROM event_Table INNER JOIN job_Name_Table ON event_Table.JOBNAME = job_Name_Table.ID INNER JOIN job_Type_Table ON event_Table.JOBTYPE = job_Type_Table.ID INNER JOIN protocol_Table ON event_Table.PROTOCOL = protocol_Table.ID;";
-            SQLiteCommand command = new SQLiteCommand(sql, _dbConnection);
+            SQLiteCommand command = new SQLiteCommand(sql, _con);
             SQLiteDataReader reader = command.ExecuteReader();
             /*
             while (reader.Read())
@@ -229,13 +173,13 @@ namespace MAD.Database
                     if (_rows.Count == 0)
                     {
                         // make new datarow
-                        _command.CommandText = "insert into DeviceTable (NODEID, ...) values ('" + _int + "', '" + node.name + "', ');";
+                        _command.CommandText = "insert into DeviceTable (ID_NODE, HOSTNAME, IP, MAC) values ('" + _int + "', '" + node.name + "', '" + node.ip + "', '" + node.mac + ");";
                         _command.ExecuteNonQuery();
                     }
                     else
                     {
                         // update
-                        _command.CommandText = "update into DeviceTable (...) values ('" + node.name + "', ');";
+                        _command.CommandText = "update DeviceTable set HOSTNAME='" + node.name + "', IP='" + node.ip + "', MAC='" + node.mac + "' where ID_NODE='" + _int + "';";
                         _command.ExecuteNonQuery();
                     }
                 }
@@ -250,11 +194,25 @@ namespace MAD.Database
         {
             InsertJobName(job.name);
 
-            int _id = GetJobNameID(job.name);
             using (SQLiteCommand _command = new SQLiteCommand(_con))
             {
-                _command.CommandText = "select * from DeviceTable where NODEID='';";
-                
+                //_command.CommandText = "select * from DeviceTable where ID_NODE='" + _id + "';";
+
+                DataTable _table = _command.ExecuteReader().GetSchemaTable();
+                DataRowCollection _rows = _table.Rows;
+
+                _command.CommandText = "insert into JobTable (ID_NODE, ID_JOB, JOBNAME, JOBTYPE, OUTSTATE, STARTTIME, STOPTIME, DELAYTIME, OUTDESC) values ('"
+                    + GetGUIDID(node.guid.ToString()) + "', '"
+                    + GetGUIDID(job.guid.ToString()) + "', '"
+                    + GetJobNameID(job.name) + "', '"
+                    + GetJobTypeID(job.type.ToString()) + "', '"
+                    + GetOutState(job.outp.outState.ToString()) + "', '"
+                    + UnixTimestampFromDateTime(job.tStart) + "', '"
+                    + UnixTimestampFromDateTime(job.tStop) + "', '"
+                    + job.tSpan.Milliseconds.ToString() + "', '";
+                    // outdesc
+                _command.ExecuteNonQuery();
+
             }
         }
 
@@ -297,6 +255,25 @@ namespace MAD.Database
             }
         }
 
+        public int GetOutState(string outstate)
+        {
+            using (SQLiteCommand _commmand = new SQLiteCommand(_con))
+            {
+                _commmand.CommandText = "select * from OutStateTable where GUID='" + outstate + "';";
+
+                DataTable _table = _commmand.ExecuteReader().GetSchemaTable();
+                DataRowCollection _rows = _table.Rows;
+
+                if (_rows.Count > 0)
+                {
+                    DataRow _row = _rows[0];
+                    return (int)_row[0];
+                }
+                else
+                    throw new Exception("OutState not found!");
+            }
+        }
+
         public bool InsertJobName(string name)
         {
             using (SQLiteCommand _commmand = new SQLiteCommand(_con))
@@ -336,7 +313,68 @@ namespace MAD.Database
             }
         }
 
+        public int GetJobTypeID(string typename)
+        {
+            using (SQLiteCommand _commmand = new SQLiteCommand(_con))
+            {
+                _commmand.CommandText = "select * from JobTypeTable where JOBTYPE='" + typename + "';";
+
+                DataTable _table = _commmand.ExecuteReader().GetSchemaTable();
+                DataRowCollection _rows = _table.Rows;
+
+                if (_rows.Count > 0)
+                {
+                    DataRow _row = _rows[0];
+                    return (int)_row[0];
+                }
+                else
+                    throw new Exception("JOBTYPE not found!");
+            }
+        }
+
+        public void InsertOnline(int online, int nodeID)
+        {
+            using (SQLiteCommand _command = new SQLiteCommand(_con))
+            {
+                _command.CommandText = "update DeviceTable set ONLINE='" + online + "' where ID_NODE='" + nodeID + "';";
+                _command.ExecuteNonQuery();
+            }
+        }
+
+        public void InsertMemo1(string memo, int nodeID)
+        {
+            using (SQLiteCommand _command = new SQLiteCommand(_con))
+            {
+                _command.CommandText = "update DeviceTable set MEMO1='" + memo + "' where ID_NODE='" + nodeID + "';";
+                _command.ExecuteNonQuery();
+            }
+        }
+
+        public void InsertMemo2(string memo, int nodeID)
+        {
+            using (SQLiteCommand _command = new SQLiteCommand(_con))
+            {
+                _command.CommandText = "update DeviceTable set MEMO2='" + memo + "' where ID_NODE='" + nodeID + "';";
+                _command.ExecuteNonQuery();
+            }
+        }
+        
         // ---
+
+        private static DateTime TimeFromUnixTimestamp(int unixTimestamp)
+        {
+            DateTime unixYear0 = new DateTime(1970, 1, 1);
+            long unixTimeStampInTicks = unixTimestamp * TimeSpan.TicksPerSecond;
+            DateTime dtUnix = new DateTime(unixYear0.Ticks + unixTimeStampInTicks);
+            return dtUnix;
+        }
+
+        public static long UnixTimestampFromDateTime(DateTime date)
+        {
+            long unixTimestamp = date.Ticks - new DateTime(1970, 1, 1).Ticks;
+            unixTimestamp /= TimeSpan.TicksPerSecond;
+            return unixTimestamp;
+        }
 
         public void Dispose()
         {
@@ -344,80 +382,3 @@ namespace MAD.Database
         }
     }
 }
-
-
-
-
-
-//at the moment useless
-/*
-public int Insert(string Command)
-{
-    SQLiteCommand cmd = new SQLiteCommand(Command, _dbConnection);
-    return cmd.ExecuteNonQuery();
-}
-
-public DataTable Select(string Command)
-{
-    SQLiteCommand cmd = new SQLiteCommand(Command, _dbConnection);
-    return cmd.ExecuteReader().GetSchemaTable();
-}
-
-public int Create(string Command)
-{
-    SQLiteCommand cmd = new SQLiteCommand(Command, _dbConnection);
-    return cmd.ExecuteNonQuery();
-}
-*/
-
-/* =========copy too main:
- 
-            DB MADstore = new DB("MAD.sqlite");
-            MADstore.ConnectDB("MAD.sqlite");
-
-            MADstore.CreateDeviceTable();
-            MADstore.CreateEventTable();
-            MADstore.CreateJobTypeTable();
-            MADstore.CreateProtocolTable();
-            MADstore.CreateJobNameTable();
-            MADstore.CreateSummaryTable();
-
-
-            MADstore.InsertToTable("Device_Table",
-                        new Insert("GUID", "0001"), 
-                        new Insert("HOSTNAME", "Router1"), 
-                        new Insert("IP", "192.168.17.17"), 
-                        new Insert("MAC", "00-50-56-C0-00-08"), 
-                        new Insert("Online", "1"), 
-                        new Insert("Memo1","NAT-Router"), 
-                        new Insert ("Memo2", "Router for Ineternet-connection"));
-            MADstore.InsertToTable("Event_Table", 
-                        new Insert("GUID", "0001"), 
-                        new Insert("JOBNAME", "0"), 
-                        new Insert("JOBTYPE", "0"), 
-                        new Insert("PROTOCOL", "0"), 
-                        new Insert("Success", "1"), 
-                        new Insert("StartTime", "12:20:20:20"), 
-                        new Insert("StopTime", "12:20:20:50"), 
-                        new Insert("DelayTime", "00:30"), 
-                        new Insert("Custom1", "IPStatus"), 
-                        new Insert("Custom2", "17"));
-            MADstore.InsertToTable("Job_Type_Table", 
-                        new Insert("ID", "0"), 
-                        new Insert("JobType", "Ping-Request"));
-            MADstore.InsertToTable("Job_Type_Table", 
-                        new Insert("ID", "1"), 
-                        new Insert("JobType", "FTP-Request"));
-            MADstore.InsertToTable("Protocol_Table", 
-                        new Insert("ID", "0"), 
-                        new Insert("Protocol", "TCP"));
-            MADstore.InsertToTable("Job_Name_Table", 
-                        new Insert("ID", "0"), 
-                        new Insert("JobName", "Ping1"));
-            MADstore.InsertToTable("Summary_Table", 
-                        new Insert("GUID", "0001"), 
-                        new Insert("DATE", "12.03.2015"), 
-                        new Insert("JOBTYPE", "0"), 
-                        new Insert("Protocol", "0"), 
-                        new Insert("OutState", "60"));
- */
